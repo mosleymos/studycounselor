67a49176d0eaf567c86b8a9943d436cb7cec0137
Louis Fourrier <louis.fourrier@gmail.com>
2015-08-28 12:19:19 +0200

    hstore problem

--- db/migrate/20150113135048_add_hstore_to_counselors.rb
+++ db/migrate/20150113135048_add_hstore_to_counselors.rb
@@ -2,13 +2,13 @@ class AddHstoreToCounselors < ActiveRecord::Migration
   
   def self.up
     execute 'CREATE EXTENSION hstore'
+    add_column :counselors, :social_url, :hstore
   end
 
   def self.down
     execute 'DROP EXTENSION hstore'
+    remove_column :counselors, :social_url, :hstore
   end
   
-  def change
-    add_column :counselors, :social_url, :hstore
-  end
+  
 end
