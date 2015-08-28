38f7e4f74e5d2d70d3034d314ec87ff7ad04415e
Louis Fourrier <louis.fourrier@gmail.com>
2015-08-28 12:09:37 +0200

    change the migration hstore

--- db/migrate/20150113135048_add_hstore_to_counselors.rb
+++ db/migrate/20150113135048_add_hstore_to_counselors.rb
@@ -1,4 +1,13 @@
 class AddHstoreToCounselors < ActiveRecord::Migration
+  
+  def self.up
+    execute 'CREATE EXTENSION hstore'
+  end
+
+  def self.down
+    execute 'DROP EXTENSION hstore'
+  end
+  
   def change
     add_column :counselors, :social_url, :hstore
   end
