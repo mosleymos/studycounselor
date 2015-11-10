deea617070a903023d0e556a7efe1e7265d1dff1
Mori <keitamori@gmail.com>
2015-11-10 10:04:09 +0100

    refactor: authorize counselors params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  $DAYS=['Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+	$HOURS=(9..23).to_a
+
+
   #before_filter :set_locale
 	
 	before_filter :configure_permitted_parameters,  if: :devise_controller?
@@ -11,7 +15,13 @@ class ApplicationController < ActionController::Base
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) << :first_name
 		devise_parameter_sanitizer.for(:sign_up) << :last_name
-
+		devise_parameter_sanitizer.for(:sign_up) << :birthdate
+		devise_parameter_sanitizer.for(:sign_up) << :address
+		devise_parameter_sanitizer.for(:sign_up) << :telephone
+		devise_parameter_sanitizer.for(:sign_up) << :jobs_description
+		devise_parameter_sanitizer.for(:sign_up) << :general_description
+		devise_parameter_sanitizer.for(:sign_up) << :school_description
+		devise_parameter_sanitizer.for(:sign_up) << :availibility_description
 	end
   
   def after_sign_in_path_for(resource)
