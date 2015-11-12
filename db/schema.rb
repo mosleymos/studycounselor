2bd2fd671a55f87bd57e414217f051ad057ee04f
Mori <keitamori@gmail.com>
2015-11-13 00:30:27 +0100

    feat: new schema with modified schools model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151112164845) do
+ActiveRecord::Schema.define(version: 20151112222436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -257,6 +257,8 @@ ActiveRecord::Schema.define(version: 20151112164845) do
     t.string   "student_number"
     t.text     "photos_videos"
     t.text     "fees"
+    t.string   "email"
+    t.string   "encrypted_password"
   end
 
   create_table "students", force: true do |t|
