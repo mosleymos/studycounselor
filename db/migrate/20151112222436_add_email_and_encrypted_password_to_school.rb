f0e8ed0351ea5a04ad8905eb904ccd29cd5f18a8
Mori <keitamori@gmail.com>
2015-11-13 14:02:31 +0100

    feat: email contact to school

--- db/migrate/20151112222436_add_email_and_encrypted_password_to_school.rb
+++ db/migrate/20151112222436_add_email_and_encrypted_password_to_school.rb
@@ -1,6 +1,5 @@
 class AddEmailAndEncryptedPasswordToSchool < ActiveRecord::Migration
   def change
 		add_column :schools, :email, :string
-		add_column :schools, :encrypted_password, :string
   end
 end
