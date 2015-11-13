e9194162b1a931878becaf5334b6f7183c1bb92d
Mori <keitamori@gmail.com>
2015-11-13 11:03:59 +0100

    feat: create a simple mailer

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,4 +1,16 @@
 class ContactsController < ApplicationController
   def message
   end
+	
+	def send_message
+		ContactMailer.sample_mail(message_params).deliver
+		redirect_to root_path , notice: 'Votre mail à été envoyé'
+	end
+
+	private
+
+	def message_params
+		params.permit(:name, :email, :message)
+	end
+
 end
