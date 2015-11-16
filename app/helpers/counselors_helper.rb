9201d11e286a1098a500addde7ddcd8729cd7077
Mori <keitamori@gmail.com>
2015-11-16 23:55:26 +0100

    feat: helpers supplementaires pour counselor

--- app/helpers/counselors_helper.rb
+++ app/helpers/counselors_helper.rb
@@ -1,2 +1,11 @@
 module CounselorsHelper
+
+	def unserialize(elem)
+		elem.gsub(/[\[\]\"\'\,]/,'').split(' ').to_a
+	end
+
+	def show_counselor_avatar(counselor)
+    counselor.image_url ? image_tag(counselor.image_url.to_s , class: "img-circle", size: "120x120" ) : image_tag('counselor.jpg', class: "img-circle", size: "120x120")  
+	end
+
 end
