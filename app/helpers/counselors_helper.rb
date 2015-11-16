64beb9ceaedde4b99e9751dc7a033e5ac7b3d823
Mori <keitamori@gmail.com>
2015-11-17 00:21:00 +0100

    fix: counselor avatar helper url

--- app/helpers/counselors_helper.rb
+++ app/helpers/counselors_helper.rb
@@ -5,7 +5,7 @@ module CounselorsHelper
 	end
 
 	def show_counselor_avatar(counselor)
-    counselor.image_url ? image_tag(counselor.image_url.to_s , class: "img-circle", size: "120x120" ) : image_tag('counselor.jpg', class: "img-circle", size: "120x120")  
+    counselor.image_url.to_s ? image_tag(counselor.image_url.to_s , class: "img-circle", size: "120x120" ) : image_tag('counselor.jpg', class: "img-circle", size: "120x120")  
 	end
 
 end
