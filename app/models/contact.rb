2d88ac7b756c42ad39ebaf3c7e8586e4411a746d
Mori <keitamori@gmail.com>
2015-11-22 14:33:12 +0100

    Contact are saved in db

---
+++ app/models/contact.rb
@@ -0,0 +1,16 @@
+# == Schema Information
+#
+# Table name: contacts
+#
+#  id         :integer          not null, primary key
+#  name       :string(255)
+#  subject    :string(255)
+#  email      :string(255)
+#  message    :text
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+class Contact < ActiveRecord::Base
+  validates :name, :email, :message, presence: true
+end
